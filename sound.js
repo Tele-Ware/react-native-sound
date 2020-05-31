35cfe473e6e5be86c49e8ca0dd1b86eb048ca091
matthewadel <matthewadel69@yahoo.com>
2020-05-31 09:11:44 +0200

    removing onError line

--- sound.js
+++ sound.js
@@ -26,7 +26,7 @@ function Sound(filename, basePath, onError, options) {
     }
   }
 
-  this.registerOnPlay = function() {
+  this.registerOnPlay = function () {
     if (this.onPlaySubscription != null) {
       console.warn('On Play change event listener is already registered');
       return;
@@ -72,15 +72,15 @@ function Sound(filename, basePath, onError, options) {
       this._loaded = true;
       this.registerOnPlay();
     }
-    onError && onError(error, props);
+    // onError && onError(error, props);
   });
 }
 
-Sound.prototype.isLoaded = function() {
+Sound.prototype.isLoaded = function () {
   return this._loaded;
 };
 
-Sound.prototype.play = function(onEnd) {
+Sound.prototype.play = function (onEnd) {
   if (this._loaded) {
     RNSound.play(this._key, (successfully) => onEnd && onEnd(successfully));
   } else {
@@ -89,7 +89,7 @@ Sound.prototype.play = function(onEnd) {
   return this;
 };
 
-Sound.prototype.pause = function(callback) {
+Sound.prototype.pause = function (callback) {
   if (this._loaded) {
     RNSound.pause(this._key, () => {
       this._playing = false;
@@ -99,7 +99,7 @@ Sound.prototype.pause = function(callback) {
   return this;
 };
 
-Sound.prototype.stop = function(callback) {
+Sound.prototype.stop = function (callback) {
   if (this._loaded) {
     RNSound.stop(this._key, () => {
       this._playing = false;
@@ -109,7 +109,7 @@ Sound.prototype.stop = function(callback) {
   return this;
 };
 
-Sound.prototype.reset = function() {
+Sound.prototype.reset = function () {
   if (this._loaded && IsAndroid) {
     RNSound.reset(this._key);
     this._playing = false;
@@ -117,7 +117,7 @@ Sound.prototype.reset = function() {
   return this;
 };
 
-Sound.prototype.release = function() {
+Sound.prototype.release = function () {
   if (this._loaded) {
     RNSound.release(this._key);
     this._loaded = false;
@@ -131,19 +131,19 @@ Sound.prototype.release = function() {
   return this;
 };
 
-Sound.prototype.getDuration = function() {
+Sound.prototype.getDuration = function () {
   return this._duration;
 };
 
-Sound.prototype.getNumberOfChannels = function() {
+Sound.prototype.getNumberOfChannels = function () {
   return this._numberOfChannels;
 };
 
-Sound.prototype.getVolume = function() {
+Sound.prototype.getVolume = function () {
   return this._volume;
 };
 
-Sound.prototype.setVolume = function(value) {
+Sound.prototype.setVolume = function (value) {
   this._volume = value;
   if (this._loaded) {
     if (IsAndroid || IsWindows) {
@@ -155,36 +155,36 @@ Sound.prototype.setVolume = function(value) {
   return this;
 };
 
-Sound.prototype.getSystemVolume = function(callback) {
-  if(!IsWindows) {
+Sound.prototype.getSystemVolume = function (callback) {
+  if (!IsWindows) {
     RNSound.getSystemVolume(callback);
   }
   return this;
 };
 
-Sound.prototype.setSystemVolume = function(value) {
+Sound.prototype.setSystemVolume = function (value) {
   if (IsAndroid) {
     RNSound.setSystemVolume(value);
   }
   return this;
 };
 
-Sound.prototype.getPan = function() {
+Sound.prototype.getPan = function () {
   return this._pan;
 };
 
-Sound.prototype.setPan = function(value) {
+Sound.prototype.setPan = function (value) {
   if (this._loaded) {
     RNSound.setPan(this._key, this._pan = value);
   }
   return this;
 };
 
-Sound.prototype.getNumberOfLoops = function() {
+Sound.prototype.getNumberOfLoops = function () {
   return this._numberOfLoops;
 };
 
-Sound.prototype.setNumberOfLoops = function(value) {
+Sound.prototype.setNumberOfLoops = function (value) {
   this._numberOfLoops = value;
   if (this._loaded) {
     if (IsAndroid || IsWindows) {
@@ -196,7 +196,7 @@ Sound.prototype.setNumberOfLoops = function(value) {
   return this;
 };
 
-Sound.prototype.setSpeed = function(value) {
+Sound.prototype.setSpeed = function (value) {
   this._speed = value;
   if (this._loaded) {
     if (!IsWindows) {
@@ -206,13 +206,13 @@ Sound.prototype.setSpeed = function(value) {
   return this;
 };
 
-Sound.prototype.getCurrentTime = function(callback) {
+Sound.prototype.getCurrentTime = function (callback) {
   if (this._loaded) {
     RNSound.getCurrentTime(this._key, callback);
   }
 };
 
-Sound.prototype.setCurrentTime = function(value) {
+Sound.prototype.setCurrentTime = function (value) {
   if (this._loaded) {
     RNSound.setCurrentTime(this._key, value);
   }
@@ -220,7 +220,7 @@ Sound.prototype.setCurrentTime = function(value) {
 };
 
 // android only
-Sound.prototype.setSpeakerphoneOn = function(value) {
+Sound.prototype.setSpeakerphoneOn = function (value) {
   if (IsAndroid) {
     RNSound.setSpeakerphoneOn(this._key, value);
   }
@@ -230,43 +230,43 @@ Sound.prototype.setSpeakerphoneOn = function(value) {
 
 // This is deprecated.  Call the static one instead.
 
-Sound.prototype.setCategory = function(value) {
+Sound.prototype.setCategory = function (value) {
   Sound.setCategory(value, false);
 }
 
-Sound.prototype.isPlaying = function() {
+Sound.prototype.isPlaying = function () {
   return this._playing;
 }
 
-Sound.enable = function(enabled) {
+Sound.enable = function (enabled) {
   RNSound.enable(enabled);
 };
 
-Sound.enableInSilenceMode = function(enabled) {
+Sound.enableInSilenceMode = function (enabled) {
   if (!IsAndroid && !IsWindows) {
     RNSound.enableInSilenceMode(enabled);
   }
 };
 
-Sound.setActive = function(value) {
+Sound.setActive = function (value) {
   if (!IsAndroid && !IsWindows) {
     RNSound.setActive(value);
   }
 };
 
-Sound.setCategory = function(value, mixWithOthers = false) {
+Sound.setCategory = function (value, mixWithOthers = false) {
   if (!IsWindows) {
     RNSound.setCategory(value, mixWithOthers);
   }
 };
 
-Sound.setMode = function(value) {
+Sound.setMode = function (value) {
   if (!IsAndroid && !IsWindows) {
     RNSound.setMode(value);
   }
 };
 
-Sound.setSpeakerPhone = function(value) {
+Sound.setSpeakerPhone = function (value) {
   if (!IsAndroid && !IsWindows) {
     RNSound.setSpeakerPhone(value)
   }
